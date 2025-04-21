96108ecc1a5c58348ab5828297053afe21849af6
Cowboy-Mike <Mike@mnanto.com>
2025-04-21 18:44:12 +0800

    added rotation

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player  # Import the Player class from the player module
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -14,10 +16,11 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+        player.update(dt)
 
 
 if __name__ == "__main__":
